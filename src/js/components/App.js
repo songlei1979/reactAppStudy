cad4095a7d72cba80dba09d3691e6083ce0f0aa9
Lei <gabriel_sl19798@hotmail.com>
2020-05-16 22:20:43 +1200

    video 4 done

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,21 +1,36 @@
 import React, {Component, Fragment} from "react";
 import ReactDom from 'react-dom';
+
+import {transitions, positions, Provider as AlertProvider} from 'react-alert'
+import AlertTemplate from "react-alert-template-basic";
+
 import Header from './Header';
 import Dashboard from "./leads/Dashboard";
+import Alerts from "./Alerts";
+
 import { Provider } from 'react-redux'
 import store from "../store";
+
+
+const alertOptions = {
+    timeout: 3000,
+    position: 'top center'
+}
 
 class App extends Component{
     render() {
         return (
             <Provider store={store}>
-                <Fragment>
-                    <Header />
-                    <div className="container">
+                <AlertProvider template={AlertTemplate}{...alertOptions}>
+                    <Fragment>
+                        <Header />
+                        <Alerts />
+                        <div className="container">
+                            <Dashboard />
+                        </div>
 
-                    </div>
-                    <Dashboard />
-                </Fragment>
+                    </Fragment>
+                </AlertProvider>
             </Provider>
         )
     }
